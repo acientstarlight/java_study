3fd6287c5870a9cb99f65b0b497789b1c8e77175
lds <2444539628@qq.com>
2021-07-30 18:05:00 +0800

    java起步调试3

---
+++ src/Java_Base/Protect.java
@@ -0,0 +1,8 @@
+package Java_Base;
+
+class Protect {
+    protected Protect(boolean value) {
+        System.out.println("访问修饰符");
+    }
+}
+
---
+++ src/Java_Base/protect2.java
@@ -0,0 +1,5 @@
+package Java_Base;
+
+public class protect2{
+
+}
