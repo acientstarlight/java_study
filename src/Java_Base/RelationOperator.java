269b2e180091ed366ea7e62c8e09751d51df5dbe
acientstarlight <2830431006@qq.com>
2021-08-05 00:49:43 +0800

    java运算符、数组、If条件语句

---
+++ src/Java_Base/RelationOperator.java
@@ -0,0 +1,23 @@
+package Java_Base;
+
+/**
+ * 关系运算符
+ */
+
+public class RelationOperator {
+
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        int c = 5;
+        System.out.println((a==b));
+        System.out.println((a<b));
+        System.out.println((a>b));
+        System.out.println((a>=b));
+        System.out.println((a<=b));
+        System.out.println((a!=b));
+    }
+
+
+
+}
